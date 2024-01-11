b2b9352b3b6d4dcbbd30bd068ad675b40c7d6187
Lucca Marchett Frozza <141957975+lmfrozza@users.noreply.github.com>
2024-01-11 18:28:26 -0300

    Add files via upload

---
+++ list project/list project/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Welcome to this DataBase!");
+Console.WriteLine("https://github.com/lmfrozza to acess README file");
+
+TestArrayInt();
+void TestArrayInt()
+{
+    int[] age = new int[5];
+    age[0] = 30;
+    age[1] = 71;
+    age[2] = 69;
+    age[3] = 34;
+    age[4] = 21;
+
+    //Console.WriteLine($"Array Lengh: {age.Length}");
+
+    int upage = 0;
+    
+    for (int i = 0; i < age.Length; i++)
+    {
+        Console.WriteLine($"Age Slot {i+1}: {age[i]}" );
+        
+        upage = upage + age[i];
+       
+    }
+    Console.WriteLine($"The Average Age is: {upage/age.Length}");
+};
